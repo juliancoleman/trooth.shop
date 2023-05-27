896e8d12b674c9f16c91e503749fe764ca1164db
Julian Coleman <jcoleman.engineering@gmail.com>
2023-05-27 10:47:15 -0700

    re-setup next project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { AppBar } from "../components/AppBar";
+
+export default function () {
+  return (
+    <>
+      <AppBar />
+    </>
+  );
+}
---
+++ src/components/AppBar.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ShoppingCart } from "@phosphor-icons/react";
+
+export const AppBar = () => (
+  <header className="flex items-center justify-between">
+    <h1>trooth</h1>
+
+    <nav></nav>
+
+    <ShoppingCart />
+  </header>
+);
