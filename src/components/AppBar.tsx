3c0ac42972ed8b326cd7fdedd6bf923144388f22
Julian Coleman <jcoleman.engineering@gmail.com>
2023-05-27 14:13:01 -0700

    chore: address type error

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,10 +1,15 @@
 "use client";
 
-import Link, { type LinkProps } from "next/link";
+import Link from "next/link";
 import { ShoppingCart } from "@phosphor-icons/react";
 import cn from "classnames";
 
 import { comfortaaBoldAlt2, nunitoSansRegular } from "../config/fonts";
+
+const baseLinkClass = cn(
+  nunitoSansRegular.className,
+  "rounded-full p-3.5 text-base text-trooth-primary-500 hover:bg-white",
+);
 
 export const AppBar = () => (
   <header className="mx-auto flex max-w-[1080px] items-center justify-between px-6 py-2.5 sm:sticky">
@@ -20,21 +25,17 @@ export const AppBar = () => (
     </Link>
 
     <nav className="hidden gap-2 sm:flex">
-      <NavLink href="/subscriptions">Subscriptions</NavLink>
-      <NavLink href="/mission">Our Mission</NavLink>
-      <NavLink href="/support">Support</NavLink>
+      <Link href="/subscriptions" className={baseLinkClass}>
+        Subscriptions
+      </Link>
+      <Link href="/mission" className={baseLinkClass}>
+        Our Mission
+      </Link>
+      <Link href="/support" className={baseLinkClass}>
+        Support
+      </Link>
     </nav>
 
     <ShoppingCart size={32} className="cursor-pointer" />
   </header>
-);
-
-export const NavLink = (props: LinkProps) => (
-  <Link
-    className={cn(
-      nunitoSansRegular.className,
-      "rounded-full p-3.5 text-base text-trooth-primary-500 hover:bg-white",
-    )}
-    {...props}
-  />
 );
