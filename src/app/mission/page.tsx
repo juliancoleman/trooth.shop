c48ba96c20fda83721959ea17233041f4354185e
Julian Coleman <jcoleman.engineering@gmail.com>
2023-07-25 19:28:25 -0700

    Update page.tsx

--- src/app/mission/page.tsx
+++ src/app/mission/page.tsx
@@ -11,22 +11,29 @@ export default function() {
       <section className="max-w-prose py-8 px-6 mx-auto">
         <h1 className={cn(nunitoRegular.className, "text-4xl mb-6")}>Trooth's Mission</h1>
         <p className={cn(nunitoSansRegular.className, "text-base mb-4")}>
-          Trooth is a family-owned business based out of Castle Rock,
-          CO. We started this company because we want to help you treat
-          your body better. <span className={nunitoSansBold.className}>Enough</span>{" "}
-          buying ineffective, harmful products from stores that lie
-          and say they're good and healthy for you.{" "}
-          <span className={nunitoSansBold.className}>Enough</span>{" "}
-          spending an arm and a leg for healthier options.
-          <span className={nunitoSansBold.className}>
-            Enough with the BS.
-          </span>
+          At Trooth, we are on a mission to revolutionize essential
+          products, starting with our industry-leading tooth powder,
+          which has been proven to be more effective than conventional
+          toothpaste at cleaning your teeth and keeping your mouth
+          healthy. We are redefining the standards for personal care.
+          We believe that the key to vibrant health lies in the power
+          of nature, and our carefully crafted products harness this
+          potential, free from harmful chemicals, to deliver safe and
+          effective solutions.
         </p>
         <p className={cn(nunitoSansRegular.className, "text-base")}>
-          Our product lines start with the teeth and mouth, but we won't
-          stop there. We vow to make a vast lineup of products that are
-          safe to eat, natural, affordable, and so good that you wouldn't
-          even think about going back to the cheaper alternatives.
+          Our commitment extends beyond just your smile&mdash;we vow
+          to make a vast lineup of products that are safe to eat,
+          natural, affordable, and so good that you wouldn't even
+          think about going back to the cheaper alternatives.
+        </p>
+        <p className={cn(nunitoSansRegular.className, "text-base")}>
+          Join us on a new era of personal care that brings vitality
+          and radiance to your life (literally). We're here to provide
+          expert guidance, and cutting-edge natural solutions. Discover
+          the Trooth and unlock the full potential of your life. Together,
+          we can create a brighter, healthier future, starting with one
+          smile at a time.
         </p>
       </section>
       <Footer />
