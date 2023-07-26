ef9220fda1ccf5da34f5b6585b3e36c5211469db
Julian Coleman <jcoleman.engineering@gmail.com>
2023-07-25 18:45:13 -0700

    Create page.tsx

---
+++ src/app/mission/page.tsx
@@ -0,0 +1,12 @@
+import { AppBar } from "@/components/AppBar";
+import { Footer } from "@/components/Footer";
+
+
+export default function() {
+  return (
+    <>
+      <AppBar />
+      <Footer />
+    </>
+  )
+}
